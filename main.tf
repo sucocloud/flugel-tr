d66dcc9663062a09001b6f0bf6398523c96d8154
Ahmed Ali <sucounix@gmail.com>
2021-10-06 08:29:58 +0200

    tr-fix

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 0.14"
+
+  backend "remote" {
+    organization = "suco-test"
+
+    workspaces {
+      name = "ghub-action"
+    }
+  }
+}
+
+
+
+
+
 provider "aws" {
   region = var.aws_region
 }
